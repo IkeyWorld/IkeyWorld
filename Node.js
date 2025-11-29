95716e40c3342937dd185958108bc0a2720d73cb
IkeyWorld <glebtarasenko4@gmail.com>
2025-11-30 02:55:29 +0300

    Create Node.js

---
+++ Node.js
@@ -0,0 +1,20 @@
+{
+  "name": "keyworld-backend",
+  "version": "1.0.0",
+  "type": "module",
+  "scripts": {
+    "start": "node app.js",
+    "dev": "nodemon app.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "cors": "^2.8.5",
+    "bcryptjs": "^2.4.3",
+    "sqlite3": "^5.1.6",
+    "uuid": "^9.0.0",
+    "express-rate-limit": "^6.7.0"
+  },
+  "devDependencies": {
+    "nodemon": "^2.0.22"
+  }
+}
